9547ffe4f547e3004625b8927f2fd529964ecedc
Matthias Pressfreund <64396187+mpfr@users.noreply.github.com>
2020-08-31 06:56:02 +0200

    Added content.

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # pkg_depts
-Determine reverse OpenBSD package dependencies
+
+Figures out which [OpenBSD](https://www.openbsd.org) packages directly or indirectly depend on the package(s) in question.
+
+For more information, just have a look at the [manpage](https://mpfr.github.io/pkg_depts/pkg_depts.1.html).
+
+> Note: Only installed packages will be considered.
+
+## How to install
+
+Make sure your user has sufficient `doas` permissions. To start, `cd` into the user's home directory, here `/home/mpfr`.
+
+```
+$ cat /etc/doas.conf
+permit nopass mpfr
+$ cd
+$ pwd
+/home/mpfr
+$
+```
+
+Get the sources downloaded and extracted.
+
+```
+$ ftp -o - https://codeload.github.com/mpfr/pkg_depts/tar.gz/master | tar xzvf -
+pkg_depts-master
+pkg_depts-master/README.md
+pkg_depts-master/docs
+pkg_depts-master/docs/mandoc.css
+pkg_depts-master/docs/pkg_depts.1.html
+pkg_depts-master/src
+pkg_depts-master/src/Makefile
+pkg_depts-master/src/pkg_depts.1
+pkg_depts-master/src/pkg_depts.sh
+$
+```
+
+Install tool and manpage.
+
+```
+$ cd pkg_depts-master/src
+$ doas make install
+install -c -o root -g bin -m 555  /home/mpfr/pkg_depts-master/src/pkg_depts.sh ...
+install -c -o root -g bin -m 444  pkg_depts.1 ...
+$
+```
+
+## How to uninstall
+
+```
+$ doas rm /usr/local/man/man1/pkg_depts.1
+$ doas rm /usr/local/sbin/pkg_depts
+$
+```
---
+++ src/pkg_depts.sh
@@ -0,0 +1,72 @@
+#!/bin/ksh
+#
+# Copyright (c) 2020 Matthias Pressfreund
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+find_dependents()
+{
+	local regex=$1
+	[[ -n $(echo ${regex} | egrep '\-[0-9]') ]] || regex="${regex}\-[0-9]"
+	echo ${cache} | egrep " ${regex}" | while read -r pkg dep; do
+		printf "%${indent}s"
+		echo "${dep} <- ${pkg}"
+		if [[ ${known#*${pkg} ${dep}} = ${known} ]]; then
+			known="${known}${pkg} ${dep}\n"
+			indent=$((indent+4))
+			find_dependents ${pkg}
+		fi
+	done
+	indent=$((indent-4))
+}
+
+TREE=false
+while getopts t arg; do
+	case ${arg} in
+	t)	TREE=true;;
+	*)	echo "usage: ${0##*/} [-t] [pkg-name ...]" >&2
+		exit 1;;
+	esac
+done
+shift $((OPTIND-1))
+
+[[ -t 0 ]] || while read -r line; do
+	cache="${cache}${line}\n"
+done
+
+[[ -n ${cache} ]] || for pkg in $(pkg_info -Aq); do
+	[[ -t 1 ]] || echo -n "${pkg} ... " >&2
+	depends=$(pkg_info -f ${pkg} | grep '^@depend' | cut -f 3 -d :)
+	for dep in ${depends}; do
+		[[ -z $@ ]] \
+			&& echo "${pkg} ${dep}" \
+			|| cache="${cache}${pkg} ${dep}\n"
+	done
+	[[ -t 1 ]] || echo 'ok' >&2
+done
+
+[[ -n ${cache} ]] || exit 0
+
+for arg do
+	indent=0
+	if ${TREE}; then
+		find_dependents ${arg}
+	else
+		dep=$(find_dependents ${arg})
+		[[ -n ${dep} ]] && deplist="${deplist}${dep}\n"
+	fi
+done
+[[ -n ${deplist} ]] && echo "${deplist%'\n'*}" | while read dep delim pkg; do
+	echo ${pkg}
+done | sort | uniq
